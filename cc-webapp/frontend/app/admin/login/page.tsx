dfdb040766104b050a9001f1e34ce601d095cf63
mamam1281 <mamam1281@gmail.com>
2025-09-07 17:33:31 +0900

    feat: Update NEXT_PUBLIC_BUILD_ID in .env.build; adjust import paths in login components; add new empty files for document and events pages

--- cc-webapp/frontend/app/admin/login/page.tsx
+++ cc-webapp/frontend/app/admin/login/page.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React from 'react';
-import App from '../App';
+import App from '../../App';
 
 // 로그인 진입: 중앙화 App을 통해 LoginScreen으로 유도
 export default function LoginPage() {
---
+++ cc-webapp/frontend/app/login/page.tsx
@@ -0,0 +1,5 @@
+import App from '../App';
+
+export default function LoginPage() {
+	return <App currentScreen="login" />;
+}
